01f7dd8253a5da00b0976f7bff753c617c89c25e
Hachiman-byte <106717707+Hachiman-byte@users.noreply.github.com>
2023-03-18 18:33:47 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,133 @@
+$(document).ready(function(){
+	$(".patatas").click(function(){	
+		for (var i = 0; i < 5; i++) {
+			$(".circle5").animate({
+				top: '10%'
+			},2600);
+			$(".circle4").animate({
+				top: '10%'
+			},2700);
+			$(".circle3").animate({
+				top: '10%'
+			},2800);
+			$(".circle2").animate({
+				top: '10%'
+			},2900);
+			$(".circle1").animate({
+				top: '10%'
+			},3000);
+			//circle set2
+			$(".circle51").animate({
+				top:'90%'
+			},2600);
+			$(".circle41").animate({
+				top:'90%'
+			},2700);
+			$(".circle31").animate({
+				top:'90%'
+			},2800);
+			$(".circle21").animate({
+				top:'90%'
+			},2900);
+			$(".circle11").animate({
+				top:'90%'
+			},3000);
+			//circle set3
+			$(".circle52").animate({
+				left:'27%'
+			},2600);
+			$(".circle42").animate({
+				left:'27%'
+			},2700);
+			$(".circle32").animate({
+				left:'27%'
+			},2800);
+			$(".circle22").animate({
+				left:'27%'
+			},2900);
+			$(".circle12").animate({
+				left:'27%'
+			},3000);
+			//circle set4
+			$(".circle53").animate({
+				left:'73%'
+			},2600);
+			$(".circle43").animate({
+				left:'73%'
+			},2700);
+			$(".circle33").animate({
+				left:'73%'
+			},2800);
+			$(".circle23").animate({
+				left:'73%'
+			},2900);
+			$(".circle13").animate({
+				left:'73%'
+			},3000);
+			//2ND animation
+			$(".circle5").animate({
+				top: '90%'
+			},2600);
+			$(".circle4").animate({
+				top: '90%'
+			},2700);
+			$(".circle3").animate({
+				top: '90%'
+			},2800);
+			$(".circle2").animate({
+				top: '90%'
+			},2900);
+			$(".circle1").animate({
+				top: '90%'
+			},3000);
+			
+			$(".circle51").animate({
+				top:'10%'
+			},2600);
+			$(".circle41").animate({
+				top:'10%'
+			},2700);
+			$(".circle31").animate({
+				top:'10%'
+			},2800);
+			$(".circle21").animate({
+				top:'10%'
+			},2900);
+			$(".circle11").animate({
+				top:'10%'
+			},3000);
+			//circle set3
+			$(".circle52").animate({
+				left:'73%'
+			},2600);
+			$(".circle42").animate({
+				left:'73%'
+			},2700);
+			$(".circle32").animate({
+				left:'73%'
+			},2800);
+			$(".circle22").animate({
+				left:'73%'
+			},2900);
+			$(".circle12").animate({
+				left:'73%'
+			},3000);
+			//circle set4
+			$(".circle53").animate({
+				left:'27%'
+			},2600);
+			$(".circle43").animate({
+				left:'27%'
+			},2700);
+			$(".circle33").animate({
+				left:'27%'
+			},2800);
+			$(".circle23").animate({
+				left:'27%'
+			},2900);
+			$(".circle13").animate({
+				left:'27%'
+			},3000);
+		}
+	});
+});
